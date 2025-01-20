9d92b65468faf9ab96b5449c4e7ecbebd3cf4689
John Roy <61093089+John-Roy123@users.noreply.github.com>
2025-01-20 02:26:04 -0500

    Connected moves from client side to backend

---
+++ Chess/src/main/java/com/john/chess/ChessRestController.java
@@ -0,0 +1,108 @@
+package com.john.chess;
+
+
+import com.john.chess.Engine.Board.Board;
+import com.john.chess.Engine.Board.Move;
+import com.john.chess.Engine.Player.MoveTransition;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChessRestController {
+
+    private Board board;
+
+
+
+
+    @PostMapping("/message")
+    public ResponseEntity<String> receiveMessage(@RequestBody MessageRequest msgReq){
+
+        if(board == null){
+            board = Board.createStartBoard();
+        }
+
+    String message = MessageRequest.getMessage();
+    String pieceName = MessageRequest.getDraggedElementName();
+    int startTile = MessageRequest.getStartPositionId();
+    int endTile = MessageRequest.getTargetTile();
+    String pieceOnTile = MessageRequest.getPieceOnTile();
+    String pieceTeam = MessageRequest.getPieceTeam();
+
+    //System.out.println(message);
+    System.out.println(pieceName + " from tile: " + startTile + " moved to tile: " + endTile + " tile is currently occupied by: " + pieceOnTile);
+    System.out.println(pieceTeam);
+
+
+
+    final Move move = Move.MoveFactory.createMove(board, startTile, endTile);
+    final MoveTransition transition = board.currentPlayer().makeMove(move);
+        if(transition.getMoveStatus().isDone()){
+            board = transition.getTransitionBoard();
+        }
+
+        System.out.println(board);
+
+    return ResponseEntity.ok("Message Received Successfully");
+    }
+
+
+
+}
+
+
+
+
+
+class MessageRequest{
+    private static String pieceOnTile;
+    private static int targetTile;
+    private static String message;
+    private static int startPositionId;
+    private static String draggedElementName;
+    private static String pieceTeam;
+
+
+
+    public static String getPieceTeam() {return pieceTeam; }
+    public void setPieceTeam(String pieceTeam) {this.pieceTeam = pieceTeam; }
+
+    public static String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public static String getDraggedElementName() {
+        return draggedElementName;
+    }
+    public void setDraggedElementName(String pieceName) {
+        this.draggedElementName = pieceName;
+    }
+
+    public static int getStartPositionId() {
+        return startPositionId;
+    }
+    public void setStartPositionId(int startPositionId) {
+        this.startPositionId = startPositionId;
+    }
+
+    public static int getTargetTile() {
+        return targetTile;
+    }
+    public void setTargetTile(int endTile) {
+        this.targetTile = endTile;
+    }
+
+    public static String getPieceOnTile() {
+        return pieceOnTile;
+    }
+    public void setPieceOnTile(String pieceOnTile) {
+        this.pieceOnTile = pieceOnTile;
+    }
+}
